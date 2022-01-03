2b55afd11600bac23350a34cae336fd4f20a93f1
LAPTOP-HTF6DU4T\yoongang <ykyeonging@naver.com>
2022-01-03 22:05:44 +0900

    시큐리티 로그인 완료

--- src/main/java/com/cos/security1/repository/UserRepository.java
+++ src/main/java/com/cos/security1/repository/UserRepository.java
@@ -6,5 +6,6 @@ import com.cos.security1.model.User;
 
 // JpaRepository 상속했기 때문에 어노테이션 필요X
 public interface UserRepository extends JpaRepository<User, Integer>{
+	public User findByUsername(String username); //JPA Query methods
 	
 }
